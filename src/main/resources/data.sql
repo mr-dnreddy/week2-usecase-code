680388a76313a34d83c44c3bf9de31079ac1a11f
D <duggempudinasarareddy@gmail.com>
2024-03-18 08:37:31 +0530

    commiting after adding data.sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO employee (first_Name, department, work_Location) VALUES ('Nasar', 'Infra', 'Hyd');
+INSERT INTO employee (first_name, department, work_location) VALUES ('Lokesh', 'IT', 'vizag');
+INSERT INTO employee (first_name, department, work_location) VALUES ('Ganesh', 'Hardware', 'Banglore');
+
